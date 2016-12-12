3202d1aadcf35b92e004e2264bb6b5de086c7f51
msenteio <mls866@nyu.edu>
2016-12-11 23:33:14 -0500

    more work on another go scene

---
+++ Assets/Scripts/levelLoaderOpen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelLoaderOpen : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+	void OnCollisionEnter2D (Collision2D hit){
+
+		Application.LoadLevel("openscene");
+		// check message upon collition for functionality working of code.
+		Debug.Log ("I am colliding with something");
+	}
+}
